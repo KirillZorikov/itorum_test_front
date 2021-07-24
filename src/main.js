2bbad94ca683ce6c6767b017c489b2aee6db5436
KirillZorikov <k.zorikov@yandex.ru>
2021-07-25 00:00:59 +0500

    update

--- src/main.js
+++ src/main.js
@@ -1,4 +1,22 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import {store} from './store'
+import router from './router'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
+import VueNextSelect from 'vue-next-select'
+import titleMixin from './mixins/title.mixin'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(router)
+app.use(store)
+app.use(VueAxios, axios)
+
+app.component('vue-select', VueNextSelect)
+
+app.mixin(titleMixin)
+
+app.mount('#app')
